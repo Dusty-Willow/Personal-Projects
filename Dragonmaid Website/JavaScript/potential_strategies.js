ad3d4c11f558c97c11a69944f366c066830a9974
Dusty-Willow <47825254+Dusty-Willow@users.noreply.github.com>
2023-03-16 17:06:58 -0400

    Added content display functionality to Potential Strategies page

--- Dragonmaid Website/JavaScript/potential_strategies.js
+++ Dragonmaid Website/JavaScript/potential_strategies.js
@@ -4,41 +4,24 @@ function toggleOverview (evt) {
 
 function displayContent (evt) {
 
-    var strat;
-    strat = parseInt(evt.target.id);
+    var eventID = $(this).attr('id');
+    var strat = parseInt(eventID.slice(-1));
+    console.log("Monkey " + strat);
+
+    $('.main_content').addClass('show_content');
+
+    $.getJSON('../JavaScript/strategies.json', function (data) {
+        console.log(data["explanation"][0]["heading"]);
 
 
-    function buildDisplayedContent (data) {
-        var element, image, heading;
-
-        // heading = $('<h3 class="sub_heading">' + + '</h3>');
-        image = $('<img src="../Resources/chamber_dragonmaid.jpg" width="300" class="left_img">');
-        // el = $('<p class="explanation_text">' + + '</p>');
-
-        data.explanation.forEach(function (entry) {
-            var contentPanel;
-            heading = $('<h3 class="sub_heading">' + entry.heading + '</h3>');
-            contentPanel = $('section.main_content', heading);
-            contentPanel.append(heading);
-        });
-
-        heading = $('<h3 class="sub_heading">' + data.explanation + '</h3>');
-        data.explanation[strat-1];
-    }
-
-    $.get('ajax/api/strategies.json', function (data) {
-
-    },JSON);
+        $('.sub_heading').text(data["explanation"][strat-1]["heading"]);
+        $('.explanation').replaceWith(data['explanation'][strat-1]['info']);
+    }).fail(function () {
+        console.log("An error has occured.");
+    });
     
-    $.get('ajax/api/strategies.json', function (data) {
-
-        $container.empty();
-        $container.append(buildDisplayedContent(data[strat]));
-        $('section.main_content').append($container);
-
-    }, JSON);
 }
 
 $('.menu_item > a').mouseover(toggleOverview);
 $('.menu_item > a').mouseout(toggleOverview);
-// $('.menu_item > a').click(displayContent)
+$('.menu_item > a').click(displayContent)
