7c68dc3a231bd4ffa8fa0f2171848e9b7f951c7a
Dusty-Willow <47825254+Dusty-Willow@users.noreply.github.com>
2023-04-18 13:35:53 -0400

    Update employee-list.component.ts

--- Angular Database/src/app/demo/employee-management/employee-list/employee-list.component.ts
+++ Angular Database/src/app/demo/employee-management/employee-list/employee-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { Employee } from '../employees/employees';
+// import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 
 @Component({
   selector: 'app-employee-list',
@@ -10,6 +11,8 @@ export class EmployeeListComponent implements OnInit {
 
   @Input() myEmployees : Employee[] = [];
   @Input() multiFilter : string | null;
+
+  // @ViewChild(MatPaginator)
 
   constructor() {}
 
